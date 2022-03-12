7752513c4120f0dacb7eb805ab3a49831428b1b8
NikitaAks <99181243+NikitaAks@users.noreply.github.com>
2022-03-12 15:51:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-   int i = 0, j=0;
-  for (j; size > j; j++) {
-    if (arr[j] == value) {
-      i++;
-    }
+   //  поместить сюда реализацию алгоритма
+  return 0; // если ничего не найдено
+  int i = 0;
+  for (int j = 0; j < size; j++) {
+    if (value == arr[j])
+      i += 1;
   }
-  if (i != 0) {
     return i;
-  }
-   else{
   return 0;
-  }
 }
